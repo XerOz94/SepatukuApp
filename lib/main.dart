d1f3b4ae27c83c9b2b5afbde35516dd6b5454325
XerOz94 <xerozoz94@gmail.com>
2024-06-03 10:24:52 +0700

    Menambah fitur akses kamera dan upload ke FirebaseStorage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:sepatuku_app/view/OnBoardingPage.dart';
-import 'package:sepatuku_app/viewmodel/sepatuku_page.dart';
 import 'package:sepatuku_app/provider/theme_provider.dart';
 
 void main() async {
@@ -25,17 +23,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => ThemeProvider(),
-        builder: (context, _) {
-          final themeProvider = Provider.of<ThemeProvider>(context);
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'SepatuKu',
-            themeMode: themeProvider.themeMode,
-            theme: MyThemes.lightTheme,
-            darkTheme: MyThemes.darkTheme,
-            home: HomePage()
+    create: (context) => ThemeProvider(),
+    builder: (context, _) {
+      final themeProvider = Provider.of<ThemeProvider>(context);
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SepatuKu',
+        themeMode: themeProvider.themeMode,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        home: HomePage(),
+        builder: (context, child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              textScaleFactor: 1.0,
+            ),
+            child: child!,
           );
         },
       );
+    },
+  );
 }
--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,33 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  ThemeMode themeMode = ThemeMode.system;
+  ThemeMode _themeMode = ThemeMode.system;
+  static const String _themeModeKey = 'theme_mode';
+
+  ThemeProvider() {
+    _loadThemeMode();
+  }
+
+  ThemeMode get themeMode => _themeMode;
 
   bool get isDarkMode {
-    if (themeMode == ThemeMode.system) {
-      final brightness = SchedulerBinding.instance.window.platformBrightness;
+    if (_themeMode == ThemeMode.system) {
+      final brightness = SchedulerBinding.instance!.window.platformBrightness;
       return brightness == Brightness.dark;
     } else {
-      return themeMode == ThemeMode.dark;
+      return _themeMode == ThemeMode.dark;
     }
   }
 
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedThemeMode = prefs.getString(_themeModeKey);
+
+    if (savedThemeMode != null) {
+      _themeMode = ThemeMode.values.firstWhere(
+        (e) => e.toString() == savedThemeMode,
+        orElse: () => ThemeMode.system,
+      );
+    }
+
+    notifyListeners(); // Perbarui UI setelah memuat tema
+  }
+
+  Future<void> _saveThemeMode(ThemeMode themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_themeModeKey, themeMode.toString());
+    notifyListeners(); // Perbarui UI setelah menyimpan tema
+  }
+
   void toggleTheme(bool isOn) {
-    themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
-    notifyListeners();
+    _themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    _saveThemeMode(_themeMode);
   }
 }
 
-class MyThemes {
-  static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.grey.shade900,
-    brightness: Brightness.dark,
-    // primaryColor: Colors.black,
-  );
-  static final lightTheme = ThemeData(
-      scaffoldBackgroundColor: Colors.white,
-      brightness: Brightness.light,
-      // primaryColor: Colors.white,
-    );
-}
+const COLOR_PRIMARY = Colors.blue;
+const COLOR_ACCENT = Colors.lightBlueAccent;
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: COLOR_PRIMARY,
+  textTheme: TextTheme(bodyText1: TextStyle(color: Colors.grey.shade900)),
+  dialogTheme: DialogTheme(
+      backgroundColor: Colors.blue.shade100,
+      // iconColor: Colors.blue,
+      surfaceTintColor: Colors.blue),
+  switchTheme: SwitchThemeData(
+      trackColor: MaterialStateProperty.all<Color>(Colors.grey.shade300),
+      thumbColor: MaterialStateProperty.all<Color>(Colors.blue),
+      trackOutlineColor: MaterialStateProperty.all<Color>(Colors.blue)),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(COLOR_PRIMARY),
+      foregroundColor: MaterialStateProperty.all(Colors.white), // Warna teks
+    ),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(color: Colors.black), // Warna label pada TextField
+    hintStyle: TextStyle(color: Colors.grey), // Warna hint pada TextField
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.blue
+      )
+    )
+  ),
+);
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: COLOR_PRIMARY,
+  switchTheme: SwitchThemeData(
+      trackColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+      thumbColor: MaterialStateProperty.all<Color>(Colors.blue),
+      trackOutlineColor: MaterialStateProperty.all<Color>(Colors.blue)),
+  dialogTheme: DialogTheme(
+      backgroundColor: Colors.black,
+      iconColor: Colors.blue,
+      surfaceTintColor: Colors.blue),
+  textTheme: TextTheme(
+    // bodyText1: TextStyle(color: Colors.black),
+    bodyText2: TextStyle(color: Colors.white),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(color: Colors.white), // Warna label pada TextField
+    hintStyle: TextStyle(color: Colors.grey), // Warna hint pada TextField
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.blue
+      )
+    )
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(COLOR_PRIMARY),
+      foregroundColor: MaterialStateProperty.all(Colors.white), // Warna teks
+    ),
+  ),
+);
